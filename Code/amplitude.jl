38925a9d8fe771e3e8a7cfdc4fbbbf361f6b758e
Loading <72392168+wangzuodong1997@users.noreply.github.com>
2021-02-15 22:53:51 +0100

    Update amplitude.jl

--- Code/amplitude.jl
+++ Code/amplitude.jl
@@ -1,14 +1,10 @@
-include("include_all.jl")
-using PyPlot
-
 # trace amplitude en fonction du pas de temps h
 
 N = 6
 Q0 = initial_positions[1:3*N] #sun and first planet
 P0 = initial_moments[1:3*N]
-
 N_amp = 20 #nb points pour tracer amplitude
-total_time = 1000 #doit suffire pour avoir au moins une periode
+total_time = 10000 #doit suffire pour avoir au moins une periode
 list_h = [i*5 for i=1:N_amp]
 list_Nh = zeros(Int, N_amp)
 for i=1:N_amp
@@ -22,18 +18,19 @@ end
 
 function plot_amplitude_symp(N_amp)
     list_amplitudes = zeros(N_amp)
-    list_h2 = zeros(Int,N_amp)
+    list_log_h = zeros(N_amp)
 
     for i=1:N_amp
         h = list_h[i]
         Nh = list_Nh[i]
         QN, PN = Symplectic(h, Nh, N, Q0, P0)
         H = energy_H(PN,QN,Nh)
-        list_h2[i] = h*h
-        list_amplitudes[i] = maximum(H) - minimum(H)
+        list_amplitudes[i] = log(maximum(H) - minimum(H))
+        list_log_h[i] = log(list_h[i])
     end
+    print((list_amplitudes[10]-list_amplitudes[1])/(list_log_h[10]-list_log_h[1]),"\n")
 
-    plot(list_h2, list_amplitudes, label = "amplitude en fonction du carré du temps", marker =".")
+    plot(list_log_h, list_amplitudes, label = "amplitude en fonction du carré du temps", marker =".")
     legend()
     title("Symplectic")
 end
@@ -44,18 +41,19 @@ end
 
 function plot_amplitude_verlet(N_amp)
     list_amplitudes = zeros(N_amp)
-    list_h2 = zeros(Int,N_amp)
+    list_log_h = zeros(N_amp)
 
     for i=1:N_amp
         h = list_h[i]
         Nh = list_Nh[i]
         QN, PN = Verlet(h, Nh, N, Q0, P0)
         H = energy_H(PN,QN,Nh)
-        list_h2[i] = h*h
-        list_amplitudes[i] = maximum(H) - minimum(H)
+        list_amplitudes[i] = log(maximum(H) - minimum(H))
+        list_log_h[i] = log(list_h[i])
     end
+    print((list_amplitudes[10]-list_amplitudes[1])/(list_log_h[10]-list_log_h[1]),"\n")
 
-    plot(list_h2, list_amplitudes, label = "amplitude en fonction du carré du temps", marker =".")
+    plot(list_log_h, list_amplitudes, label = "amplitude en fonction du carré du temps", marker =".")
     legend()
     title("Verlet")
 end
@@ -63,7 +61,7 @@ end
 #plot_amplitude_verlet(N_amp)
 
 
-function plot_amplitude_modified_energy_Symplectic(N_amp)
+function plot_amplitude_modified_energy(N_amp)
     list_amplitudes = zeros(N_amp)
     list_log_h = zeros(N_amp)
 
@@ -75,7 +73,8 @@ function plot_amplitude_modified_energy_Symplectic(N_amp)
         list_amplitudes[i] = log(maximum(H) - minimum(H))
         list_log_h[i] = log(list_h[i])
     end
-
+    print((list_amplitudes[10]-list_amplitudes[1])/(list_log_h[10]-list_log_h[1]),"\n")
+    
     plot(list_log_h, list_amplitudes, label = "log amplitude modified energy", marker =".")
     xlabel("log time step")
     legend()
@@ -84,7 +83,7 @@ end
 
 
 
-function plot_amplitude_modified_energy_Verlet(N_amp)
+function plot_amplitude_modified_energy_V(N_amp)
     list_amplitudes = zeros(N_amp)
     list_log_h = zeros(N_amp)
 
@@ -96,6 +95,7 @@ function plot_amplitude_modified_energy_Verlet(N_amp)
         list_amplitudes[i] = log(maximum(H) - minimum(H))
         list_log_h[i] = log(list_h[i])
     end
+    print((list_amplitudes[10]-list_amplitudes[1])/(list_log_h[10]-list_log_h[1]),"\n")
 
     plot(list_log_h, list_amplitudes, label = "log amplitude modified energy", marker =".")
     xlabel("log time step")
@@ -103,7 +103,4 @@ function plot_amplitude_modified_energy_Verlet(N_amp)
     title("Verlet")
 end
 
-figure()
-plot_amplitude_modified_energy_Symplectic(N_amp)
-figure()
-plot_amplitude_modified_energy_Verlet(N_amp)
+
